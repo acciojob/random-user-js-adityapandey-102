27b6728eb1681b8fbfef139049c5fe94f16b8e05
Aditya Pandey <programmer.adityapandey1234@gmail.com>
2023-04-30 01:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+let UserData={}
+const handleGetUser=()=>{
+	let user= await fetch("https://randomuser.me/api/");
+	let userData=await user.json();
+	console.log(userData);
+	UserData=userData;
+	document.getElementById("image").innerURL=UserData.image;
+	document.getElementById("userName").innerText=UserData.name;
+
+}
+const handleAge=()=>{
+	document.getElementById("content").innerText=UserData.age;
+}
+const handleEmail=()=>{
+	document.getElementById("content").innerText=UserData.email;
+}
+const handlePhone=()=>{
+	document.getElementById("content").innerText=UserData.Phone;
+}
